cf74cdc8919f4fe7b927d1ad2aa18bd1e5778e72
Szymon Idec <szymonidec@student.agh.edu.pl>
2023-04-11 22:55:39 +0200

    add order

--- src/AddOrder.js
+++ src/AddOrder.js
@@ -3,8 +3,26 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import addOrder from "./logic/addOrder";
+import { Timestamp } from "firebase/firestore";
 
 const AddOrder = () => {
+
+  const handleSubmit = () => {
+    const order = {
+      carName: "Fiat 126p",
+      year: 1980,
+      clientName: "Jan Kowalski",
+      cost: Number(10 + Math.random() * (10000 - 10)).toFixed(2),
+      profit: Number(10 + Math.random() * (1000 - 10)).toFixed(2),
+      status: "IN_PROGRESS",
+      dueDate: Timestamp.fromDate(new Date("2023-04-24 12:50")),
+      description: "Dawno, dawno, dawno temu za siedmiogórozwierzogrodem żył Shrek. k.",
+    };
+    addOrder(order);
+    alert("pyk pyk");
+  };
+
   return (
     <Card>
       <Card.Header>
@@ -14,7 +32,7 @@ const AddOrder = () => {
             <Card.Subtitle className="text-muted">...</Card.Subtitle>
           </Col>
           <Col>
-            <Button className="float-end">Zatwierdź</Button>
+            <Button onClick={handleSubmit} className="float-end">Zatwierdź</Button>
           </Col>
         </Row>
       </Card.Header>
