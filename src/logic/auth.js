7258bd14389438f5bbecfa40705a924203f17332
Carbon225 <carbon225@protonmail.com>
2023-04-11 21:53:51 +0200

    protected route

---
+++ src/logic/auth.js
@@ -0,0 +1,9 @@
+import { auth } from "./fb";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+export const useUserPermissionLevel = () => {
+  const [user, loading, error] = useAuthState(auth);
+  if (loading || error || !user) return [null, loading];
+  return ["MECHANIC", false];
+  // TODO: check mechanic permission level
+};
