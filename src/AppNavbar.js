9abb475e472feb78998b6bdd9246c833406e8780
Carbon225 <carbon225@protonmail.com>
2023-04-11 15:23:56 +0200

    add debug navbar

--- src/AppNavbar.js
+++ src/AppNavbar.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 
 const AppNavbar = () => (
   <Navbar variant={"dark"} bg={"primary"}>
     <Container>
       <Navbar.Brand>IO IO IO</Navbar.Brand>
+      <Navbar.Text>Debug only navbar</Navbar.Text>
+      <Navbar.Collapse>
+        <Nav className="me-auto">
+          <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/mechanic/home">
+            <Nav.Link>Mechanic Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/client/order">
+            <Nav.Link>Client Order</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
       <Navbar.Text>YES WE CAN</Navbar.Text>
     </Container>
   </Navbar>
