e5ca7150e60cbc50bf4f48f72c5cfebc4246b01c
Carbon225 <carbon225@protonmail.com>
2023-04-13 15:11:01 +0200

    add order form

--- src/AddOrder.js
+++ src/AddOrder.js
@@ -4,60 +4,65 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import { addOrder } from "./model/order";
 
 const AddOrder = () => {
 
-  const handleSubmit = useCallback(async () => {
+  const handleSubmit = useCallback(async e => {
+    e.preventDefault();
     const order = {
       car: {
-        model: "zupa z pierogami",
-        year: 1980,
+        model: e.target.elements[0].value,
+        year: Number(e.target.elements[1].value),
       },
       client: {
-        firstName: "Ty",
-        lastName: "Carbon",
+        firstName: e.target.elements[2].value,
+        lastName: e.target.elements[3].value,
       },
-      cost: Number(10 + Math.random() * (10000 - 10)).toFixed(2),
-      profit: Number(10 + Math.random() * (1000 - 10)).toFixed(2),
+      cost: Number(e.target.elements[4].value),
+      profit: Number(e.target.elements[5].value),
       status: "IN_PROGRESS",
-      dueDate: new Date("2023-04-24 12:50"),
-      description: "Dawno, dawno, dawno temu za siedmiogórozwierzogrodem żył Shrek. k.",
+      dueDate: new Date(e.target.elements[6].value),
+      description: e.target.elements[7].value,
     };
     await addOrder(order);
   });
 
   return (
-    <Card>
-      <Card.Header>
-        <Row className="align-items-center">
-          <Col>
-            <Card.Title>...</Card.Title>
-            <Card.Subtitle className="text-muted">...</Card.Subtitle>
-          </Col>
-          <Col>
-            <Button onClick={handleSubmit} className="float-end">Zatwierdź</Button>
-          </Col>
-        </Row>
-      </Card.Header>
-      <Card.Body>
-        <Card.Text>
-          <strong>Klient:</strong> ...
-        </Card.Text>
-        <Card.Text>
-          <strong>Koszt naprawy:</strong> ... zł
-        </Card.Text>
-        <Card.Text>
-          <strong>Zysk:</strong> ... zł
-        </Card.Text>
-        <Card.Text>
-          <strong>Termin realizacji:</strong> ...
-        </Card.Text>
-        <Card.Text>
-          <strong>Opis:</strong> ... ... ...
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <Form onSubmit={handleSubmit}>
+      <Card>
+        <Card.Header>
+          <Row>
+            <Col>
+              <Form.Label>Dane samochodu</Form.Label>
+              <Form.Control type="text" placeholder="Model" />
+              <Form.Control type="text" placeholder="Rok produkcji" />
+            </Col>
+            <Col>
+              <Button variant="primary" type="submit" className="float-end">Zatwierdź</Button>
+            </Col>
+          </Row>
+        </Card.Header>
+        <Card.Body>
+          <Form.Label>Dane klienta</Form.Label>
+          <Form.Control type="text" placeholder="Imię" />
+          <Form.Control type="text" placeholder="Nazwisko" />
+          
+          <Form.Label className="mt-3">Koszt naprawy</Form.Label>
+          <Form.Control type="text" placeholder="Koszt naprawy" />
+          
+          <Form.Label className="mt-3">Zysk</Form.Label>
+          <Form.Control type="text" placeholder="Zysk" />
+          
+          <Form.Label className="mt-3">Termin realizacji</Form.Label>
+          <Form.Control type="text" placeholder="Termin realizacji" />
+          
+          <Form.Label className="mt-3">Opis</Form.Label>
+          <Form.Control type="text" placeholder="Opis" />
+        </Card.Body>
+      </Card>
+    </Form>
   );
 };
 
