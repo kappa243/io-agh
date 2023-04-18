aa2cc1cad6760bfb68e2bd174cef59febc3eb4a9
Szymon Idec <szymonidec@student.agh.edu.pl>
2023-04-18 20:29:38 +0200

    status button

--- src/OrderDetails.js
+++ src/OrderDetails.js
@@ -3,9 +3,20 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import { orderStatusText } from "./model/order";
+import { orderStatusColor, orderStatusText } from "./model/order";
+import { ButtonGroup, Dropdown } from "react-bootstrap";
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
+import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
+import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
 
 const OrderDetails = ({ order }) => {
+
+
+  const handleStatusChange = (status) => {
+    // TODO backend call here
+    order.status = status; // to be removed after backend integration
+  };
+
   return (
     <Card>
       <Card.Header>
@@ -16,6 +27,16 @@ const OrderDetails = ({ order }) => {
           </Col>
           <Col className="d-flex flex-row-reverse gap-2">
             <Button>Edytuj</Button>
+            <Dropdown as={ButtonGroup} style={{ width: "180px" }}>
+              <DropdownToggle variant={orderStatusColor[order.status]} className="w-100">
+                {orderStatusText[order.status]}
+              </DropdownToggle>
+              <DropdownMenu className="w-100">
+                {Object.keys(orderStatusText).map((status, index) => (
+                  <DropdownItem onClick={() => handleStatusChange(status)} key={index} active={status === order.status}>{orderStatusText[status]}</DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
           </Col>
         </Row>
       </Card.Header>
@@ -28,9 +49,6 @@ const OrderDetails = ({ order }) => {
         </Card.Text>
         <Card.Text>
           <strong>Zysk:</strong> {Number(order.profit).toFixed(2)} zł
-        </Card.Text>
-        <Card.Text>
-          <strong>Status:</strong> {orderStatusText[order.status]}
         </Card.Text>
         <Card.Text>
           <strong>Termin realizacji:</strong> {order.dueDate.toLocaleDateString("pl-PL")}
