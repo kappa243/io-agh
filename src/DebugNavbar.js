7c63d6d1110be208716b096dc125cefd6c81823b
Szymon Idec <szymonidec@student.agh.edu.pl>
2023-05-05 21:15:00 +0200

    exit debug

--- src/DebugNavbar.js
+++ src/DebugNavbar.js
@@ -7,30 +7,35 @@ import { useUserEmail } from "./logic/auth";
 
 const DebugNavbar = () => {
   const email = useUserEmail();
+  const [visibility, setVisibility] = React.useState(true);
 
   return (
-    <Navbar bg="warning">
-      <Container>
-        <Navbar.Brand>IO IO IO</Navbar.Brand>
-        <Navbar.Text>Debug only navbar</Navbar.Text>
-        <Navbar.Collapse>
-          <Nav className="me-auto">
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/mechanic/home">
-              <Nav.Link>Mechanic Home</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/client/order">
-              <Nav.Link>Client Order</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Text>{email}</Navbar.Text>
-        <Navbar.Text className="ms-4">YES WE CAN</Navbar.Text>
-      </Container>
-    </Navbar>
-  );
+    <>
+      {visibility &&
+        <Navbar bg="warning">
+          <Container>
+            <Navbar.Brand>IO IO IO</Navbar.Brand>
+            <Navbar.Text>Debug only navbar</Navbar.Text>
+            <Navbar.Collapse>
+              <Nav className="me-auto">
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/mechanic/home">
+                  <Nav.Link>Mechanic Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/client/order">
+                  <Nav.Link>Client Order</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+            <Navbar.Text>{email}</Navbar.Text>
+            <Navbar.Text className="ms-4">YES WE CAN</Navbar.Text>
+          </Container>
+          <span onClick={() => setVisibility(false)} className="dark text-center fs-2 me-2" style={{ width: "2rem", height: "2rem", lineHeight: "100%", cursor: "pointer" }} >&times;</span>
+        </Navbar>
+      }
+    </>);
 };
 
 export default DebugNavbar;
