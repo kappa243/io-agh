a1c391b149775a29eac1e54f4a003071b34a5d34
Carbon225 <carbon225@protonmail.com>
2023-05-10 18:50:36 +0200

    kill navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Inter } from 'next/font/google';
-import DebugNavbar from "@/components/DebugNavbar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <DebugNavbar />
         {children}
       </body>
     </html>
